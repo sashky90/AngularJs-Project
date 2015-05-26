5237be9a596211e76a3df3463458d354c967b823
sashky90 <sashky@ymail.com>
2015-05-26 14:24:27 +0300

    feed Post controller added

--- app/js/controllers/postControllers/feedPostsController.js
+++ app/js/controllers/postControllers/feedPostsController.js
@@ -34,20 +34,63 @@ socialNetwork.controller('feedPostsController', function ($scope, postsService, 
         )
     };
 
-    //$scope.loadPosts = function() {
-    //    if ($location.path().indexOf('/users/') > -1) {
-    //        $scope.getUserWallPosts($routeParams.username);
-    //    } else if ($location.path().indexOf('/home') > -1) {
-    //        $scope.getNewsFeedPosts();
-    //    }
-    //};
-    //
-    //$scope.loadPosts();
+    $scope.loadPosts = function() {
+        if ($location.path().indexOf('/users/') > -1) {
+            $scope.getUserWallPosts($routeParams.username);
+        } else if ($location.path().indexOf('/home') > -1) {
+            $scope.getNewsFeedPosts();
+        }
+    };
 
-    //$scope.addNewPost = function (postContent) {
-    //    var contentToPost = {
-    //        postContent: postContent,
-    //        username: $scope.
-    //    }
-    //}
+    $scope.loadPosts();
+
+    $scope.addNewPost = function (postContent) {
+        var contentToPost = {
+            postContent: postContent,
+            username: $scope.wallOwner.username
+        };
+
+        postsService.addNewPost(contentToPost).then(
+            function success(data) {
+                notifyService.showInfo('Successfully posted to' + $scope.wallOwner + ' wall');
+                $scope.getUserWallPosts($routeParams.username);
+            },
+            function error(error) {
+                notifyService.showError('Cannot proceed data request');
+            }
+        )
+    };
+
+    $scope.deletePost = function(postId) {
+        postsService.deletePost(postId).then(
+            function success(result) {
+                notifyService.showInfo("Successfully deleted post.");
+                $scope.loadPosts();
+            },
+            function error(error) {
+                notifyService.showError("Error while deleting post.", error);
+            })
+    };
+
+    $scope.likePost = function(postId) {
+        postsService.likePost(postId).then(
+            function success(result) {
+                notifyService.showInfo("Successfully liked post.");
+                $scope.loadPosts();
+            },
+            function error(error) {
+                notifyService.showError("Error while liking post.", error);
+            });
+    };
+
+    $scope.unlikePost = function(postId) {
+        postsService.unlikePost(postId).then(
+            function success(result) {
+                notifyService.showInfo("Successfully unliked post.");
+                $scope.loadPosts();
+            },
+            function error(error) {
+                notifyService.showError("Error while unliking post.");
+            });
+    };
 });
--- app/js/controllers/userControllers/foreignUserHeaderController.js
+++ app/js/controllers/userControllers/foreignUserHeaderController.js
@@ -1,3 +1,23 @@
-/**
- * Created by Sasho on 5/22/2015.
- */
+'use strict';
+
+socialNetwork.controller('UserHeaderController', function($scope, $routeParams, authentication, userService, notifyService) {
+    var user = authentication.getUserData().username;
+
+    $scope.getUserData = function(wallOwnerUsername) {
+        userService.getUserFullData(wallOwnerUsername).then(
+            function success(wallOwnerProfile) {
+
+                $scope.postOptionAvailable = wallOwnerProfile.data.isFriend ||
+                wallOwnerProfile.data.username == user;
+
+                $scope.friendsPreviewAvailable = wallOwnerProfile.data.isFriend;
+
+                $scope.wallOwner = wallOwnerProfile.data;
+            },
+            function error(error) {
+                notifyService.showError("Error with data request.", error);
+            });
+    };
+
+    $scope.getUserData($routeParams.username);
+});
