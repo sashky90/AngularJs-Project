5a84b3c087f7cc4ce678775c6359bb80bc9d93a2
sashky90 <sashky@ymail.com>
2015-05-30 20:06:17 +0300

    Comment Services Implemented

--- app/js/services/commentService.js
+++ app/js/services/commentService.js
@@ -1,3 +1,64 @@
-/**
- * Created by Sasho on 5/22/2015.
- */
+socialNetwork.factory('commentsData', function($http, baseServiceUrl, authentication) {
+    function getPostComments(postId) {
+        return $http({
+            method: 'GET',
+            url: baseServiceUrl + 'posts/' + postId + '/comments',
+            headers: authentication.getHeaders()
+        });
+    }
+
+    function addPostComment(postId, commentContent) {
+        return $http({
+            method: 'POST',
+            url: baseServiceUrl + 'posts/' + postId + '/comments',
+            headers: authentication.getHeaders(),
+            data: {
+                commentContent: commentContent
+            }
+        });
+    }
+
+    function deletePostComment(postId, commentId) {
+        return $http({
+            method: 'DELETE',
+            url: baseServiceUrl + 'posts/' + postId + '/comments/' + commentId,
+            headers: authentication.getHeaders()
+        });
+    }
+
+    function editPostComment(postId, commentId, commentContent) {
+        return $http({
+            method: 'PUT',
+            url: baseServiceUrl + 'posts/' + postId + '/comments/' + commentId,
+            headers: authentication.getHeaders(),
+            data: {
+                commentContent: commentContent
+            }
+        });
+    }
+
+    function likePostComment(postId, commentId) {
+        return $http({
+            method: 'POST',
+            url: baseServiceUrl + 'posts/' + postId + '/comments/' + commentId + '/likes',
+            headers: authentication.getHeaders()
+        });
+    }
+
+    function unlikePostComment(postId, commentId) {
+        return $http({
+            method: 'DELETE',
+            url: baseServiceUrl + 'posts/' + postId + '/comments/' + commentId + '/likes',
+            headers: authentication.getHeaders()
+        });
+    }
+
+    return {
+        getPostComments: getPostComments,
+        addPostComment: addPostComment,
+        deletePostComment: deletePostComment,
+        editPostComment: editPostComment,
+        likePostComment: likePostComment,
+        unlikePostComment: unlikePostComment
+    }
+});
